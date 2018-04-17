eda9697c3ae3310b6ab970dd118356f934d355b8
Martin Spacek <git@mspacek.mm.st>
2018-04-17 02:21:30 +0200

    Add class 02 notes

---
+++ 02_Python_basics_2/example_functions.py
@@ -0,0 +1,14 @@
+def add(x, y):
+     """Return x + y"""
+     result = x + y
+     return result
+
+def subtract(x, y):
+     """Return x - y"""
+     result = x - y
+     return result
+
+def add3(x, y, z=0):
+     """Return x + y + z"""
+     result = x + y + z
+     return result
